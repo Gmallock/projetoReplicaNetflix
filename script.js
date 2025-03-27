1262b20a2e588ff0e57e667a561893f940eec37c
Mallock <45922516+Gmallock@users.noreply.github.com>
2025-03-27 00:42:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("language-select").addEventListener("change", function() {
+    if (this.value === "en"){
+     window.location.href = "indexNetflixEnglish.html";
+    } 
+ });
+
+ document.querySelector(".submit-btn").addEventListener("click", function(){
+     let emailInput = document.querySelector(".email-input");
+     let errorMessage = document.querySelector(".error-message");
+     let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+     if(!emailPattern.test(emailInput.value)){
+         emailInput.classList.add("error");
+         errorMessage.style.display = "block";
+     } else{
+         emailInput.classList.remove("error");
+         errorMessage.style.display = "none";
+         window.location.href = "https://www.netflix.com/signup/password?locale=pt-BR";
+     }
+ }); 
